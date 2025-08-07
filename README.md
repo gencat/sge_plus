56b8868923d770c1c44659cff0a6cd7a32574b9c
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2025-08-07 11:41:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# sge_plus
+
+Free Open-Source participatory democracy, citizen participation and open government for cities and organizations
+
+This is the open-source repository for SGE+. SGE+ is based on [Decidim](https://github.com/decidim/decidim).
+
+## Development
+
+```bash
+bundle install
+nvm use 18.17.1
+npm install
+bin/rails db:create db:schema:load
+bin/rails db:seed
+bin/rails s
+```
+## Testing
+
+Run `bin/rake decidim:generate_external_test_app` to generate a dummy application to test both the application and the modules.
+
+Require missing factories in `spec/factories.rb`
+
+Add `require "rails_helper"` to your specs and execute them from the root directory, i.e.:
+
+```bash
+bundle exec rspec --backtrace
+```
---
+++ spec/system/homepage_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Homepage" do
+  include Decidim::SanitizeHelper
+
+  let!(:organization) do
+    create(
+      :organization,
+      name: {ca: "SGE Plus"},
+      default_locale: :en,
+      available_locales: [:ca, :en, :es]
+    )
+  end
+  let!(:hero) do
+    create(:content_block, organization: organization, scope_name: :homepage, manifest_name: :hero, settings: {
+             "welcome_text_ca" => "Benvinguda a SGE+"
+           })
+  end
+  let!(:sub_hero) do
+    create(:content_block, organization: organization, scope_name: :homepage, manifest_name: :sub_hero)
+  end
+
+  before do
+    switch_to_host(organization.host)
+    visit decidim.root_path(locale: I18n.locale)
+  end
+
+  it "loads and shows organization name and main blocks" do
+    visit decidim.root_path
+
+    expect(page).to have_content("SGE+")
+    within "section.hero__container .hero__title" do
+      expect(page).to have_content("Benvinguda a SGE+")
+    end
+    within "section.home__section" do
+      subhero_msg = translated(organization.description).gsub(%r{</p>\s+<p>}, "<br><br>").gsub(%r{<p>(((?!</p>).)*)</p>}mi, '\\1').gsub(%r{<script>(((?!</script>).)*)</script>}mi, '\\1')
+      expect(page).to have_content(subhero_msg)
+    end
+  end
+end
