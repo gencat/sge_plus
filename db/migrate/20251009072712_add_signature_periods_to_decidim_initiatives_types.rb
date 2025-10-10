57eea5fc70e7a09c67bf3bff1e92e259f0ed5bda
Ruben <ruben.bg@coditramuntana.com>
2025-10-10 10:49:12 +0200

    Fix linters

--- db/migrate/20251009072712_add_signature_periods_to_decidim_initiatives_types.rb
+++ db/migrate/20251009072712_add_signature_periods_to_decidim_initiatives_types.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddSignaturePeriodsToDecidimInitiativesTypes < ActiveRecord::Migration[7.0]
   def change
     add_column :decidim_initiatives_types, :signature_period_start, :datetime
