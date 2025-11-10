b3bbb943d3a4159164cf785070998cd63821405d
Laura Jaime <laura.jv@coditramuntana.com>
2025-11-10 14:22:51 +0100

    Refactor CandidacyActivityCell by removing unnecessary module nesting

--- decidim-signature_collection/app/cells/decidim/candidacy_activity_cell.rb
+++ decidim-signature_collection/app/cells/decidim/candidacy_activity_cell.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 module Decidim
-  module SignatureCollection
-    # A cell to display when an candidacy has been published.
-    class CandidacyActivityCell < ActivityCell
-      def title
-        I18n.t "decidim.signature_collection.last_activity.new_candidacy"
-      end
+  # A cell to display when an candidacy has been published.
+  class CandidacyActivityCell < ActivityCell
+    def title
+      I18n.t "decidim.signature_collection.last_activity.new_candidacy"
     end
   end
 end
